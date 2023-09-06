3aec88e0f4391888331300bde29e04a049071512
YuhangZheng <zyhnesmrtelnost@foxmail.com>
2023-09-06 16:10:52 +0800

    svc addr

--- cli.go
+++ cli.go
@@ -35,13 +35,10 @@ func SagaBarrier() {
 	addReq := &busis.AddReq{UserId: 1, Money: 50}
 	delReq := &busis.AddReq{UserId: 2, Money: 50}
 	saga := dtmgrpc.NewSagaGrpc(dtmSvc, gid).
-		Add("k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/AddMoney", "k8s://ohmyfans/dtm-rpc:9091/busis.Busis/DelMoney", addReq).
-		Add("k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/DelMoney", "k8s://ohmyfans/dtm-rpc:9091/busis.Busis/AddMoney", delReq)
+		Add("k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/AddMoney", "k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/DelMoney", addReq).
+		Add("k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/DelMoney", "k8s://ohmyfans/dtm-rpc-svc:9091/busis.Busis/AddMoney", delReq)
 
 	err := saga.Submit()
 	logger.FatalIfError(err)
 	fmt.Println(saga.Gid)
-}
-
-func Saga() {
 }
